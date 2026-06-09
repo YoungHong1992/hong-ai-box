2f4f38cc10ec7f526304b49f2b01e222fc50b56e
root <root@ser8499954121.local>
2026-06-09 11:23:25 +0000

    chore: bump version to v0.1.1

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,7 +2,7 @@ package version
 
 // Version is the current version of hongaibox.
 // It can be overridden at build time via ldflags.
-var Version = "0.1.0"
+var Version = "0.1.1"
 
 // Name is the application name.
 const Name = "hongaibox"
